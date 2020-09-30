14bb342805953773c104d03e59f7e054f0a07a4a
Notavone <notavone@gmail.com>
2020-09-30 10:39:23 +0200

    Passage sur apache

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Title</title>
+</head>
+<body class="bg-dark text-white">
+<nav class="navbar shadow-lg navbar-expand-lg sticky-top navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><img class="rounded-circle" width="50" height="50" src="./assets/img/150x150.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><a class="nav-link" href="#">ACCUEIL</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">PAGE 1</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">PAGE 2</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">PAGE 3</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">PAGE 4</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">PAGE 5</a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    ÉQUIPE
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Nathan BLIND HEITZ</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Émile BATKOWSKI</a>
+                    <a class="dropdown-item" href="#">Vassily DELMAS</a>
+                    <a class="dropdown-item" href="#">Léo HUGONNOT</a>
+                    <a class="dropdown-item" href="#">Jules DELERS</a>
+                </div>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="#">NOUS CONTACTER</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    <div class="row">
+
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!--
+__________________________░░__________________░░____░░░░░░__░░░░░░░░
+______░░______________________████████______░░░░░░░░░░░░░░░░░░░░░░░░
+________░░░░____░░__________██________██__░░____░░░░________________
+__________________________██░░______▒▒░░██__________________________
+__________________░░__░░██░░░░░░____░░░░▒▒██________________________
+________________________██░░░░░░____░░░░░░██________________________
+______________________██░░░░░░░░______░░░░░░██______________________
+______________________██░░░░░░______________██______________________
+__░░______________░░██__░░__░░__▒▒▒▒▒▒░░______██____________________
+░░░░░░░░░░░░░░░░░░░░██__░░____▒▒▒▒▒▒▒▒▒▒____░░██░░░░░░░░░░░░░░░░░░░░
+▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒▒▒██░░░░__░░▒▒▒▒▒▒▒▒▒▒______██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░░░░__▒▒▒▒▒▒▒▒▒▒__▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░░____▒▒▒▒▒▒__▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██░░░░____________▒▒▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██______░░____░░▒▒██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████░░░░__░░████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+-->
+</body>
+</html>
