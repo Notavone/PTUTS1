84413cf30f7a585cfa686edf02c13044b904c6eb
Notavone <notavone@gmail.com>
2020-12-17 12:03:03 +0100

    ajout de la partie de vassily (page de droit)

--- pages/droit.php
+++ pages/droit.php
@@ -12,73 +12,85 @@ $page = "ecodroit";
 <main class="container">
     <div class="row">
         <div class="col-12">
-            <h1 class="border-bottom pb-1 mb-3">Titre de niveau 1</h1>
-            <div class="text-justify">
-                <img src="/public/images/1920x1080.png" class="float-right img-fluid w-50 ml-3 rounded-lg" alt="">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra eros et dui tincidunt
-                    pulvinar.
-                    Sed sed urna laoreet tellus feugiat mollis ut quis leo. Aenean gravida, urna at molestie feugiat,
-                    est
-                    sapien
-                    malesuada erat, non rutrum arcu leo sit amet lacus. Integer mollis sapien at sagittis suscipit.
-                    Donec.
-                </p>
-                <p>
-                    Fusce a odio eget neque interdum euismod. Sed quis venenatis elit. Ut tincidunt sollicitudin neque a
-                    eleifend. Duis a risus at ligula congue consectetur. Suspendisse potenti. Donec lectus dolor,
-                    tristique
-                    non
-                    semper non, viverra sit amet tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-                    vulputate
-                    convallis urna, cursus tristique enim pulvinar nec.
-                </p>
-            </div>
-            <h2 class="border-bottom pb-1 mb-2">Titre de niveau 2</h2>
-            <div class="text-justify">
-                <p>
-                    Aliquam erat volutpat. Nunc a metus pellentesque, condimentum massa vitae, ornare ex. Vivamus felis
-                    enim,
-                    fringilla nec ultricies eget, rutrum fermentum ligula. Donec nec mattis turpis. Praesent tempus nisl
-                    eu
-                    tellus vehicula, sed varius lacus pretium. Cras rhoncus sit amet risus id varius. Nunc et augue
-                    vitae
-                    neque
-                    iaculis porttitor eget et sapien.
-                </p>
-                <p>
-                    Donec tristique sodales condimentum. Pellentesque in elit pharetra, tincidunt lectus sed, accumsan
-                    nisl.
-                    Aliquam erat volutpat. Donec egestas mi nunc, lobortis lobortis nisi viverra id. Aliquam ante urna,
-                    porta
-                    non ligula at, cursus efficitur lorem. Pellentesque nec vestibulum nulla. Mauris quis lorem enim.
-                    Pellentesque efficitur egestas ante, quis bibendum nibh tincidunt at. Vestibulum lacus ligula,
-                    pellentesque
-                    vitae libero sit amet, tincidunt fermentum quam. Vestibulum id pharetra leo. Proin ac dictum orci.
-                    Praesent
-                    vestibulum odio non rhoncus aliquet. Sed eu ultrices leo. Nunc congue sem eu eros lacinia egestas.
-                    Aenean et
-                    libero nec velit fermentum tincidunt eget vitae libero.
-                </p>
-            </div>
-            <h2 class="border-bottom pb-1 mb-2">Titre de niveau 2</h2>
-            <img src="/public/images/1920x1080.png" class="float-left img-fluid w-50 mr-3 rounded-lg" alt="">
-            <div class="text-justify">
-                <p>
-                    Aenean porttitor, nulla quis ornare dapibus, magna odio pretium tortor, non convallis est lorem eu
-                    mauris. Phasellus et ullamcorper libero. Morbi vestibulum mi ac libero sagittis maximus. Sed
-                    ultrices, eros ut tincidunt ultrices, diam tellus feugiat sem, eu mollis felis metus eu lectus. Nam
-                    consequat ipsum arcu, maximus bibendum sem vestibulum eget.
-                </p>
-                <p>
-                    Cras est urna, mattis quis varius sed, egestas eu felis. Suspendisse potenti. Fusce lectus lacus,
-                    porttitor quis mattis egestas, ornare sed mauris. Curabitur eget elementum est. Proin vestibulum
-                    hendrerit lacus, non sagittis erat auctor nec. Vestibulum posuere, diam a
-                    pellentesque mollis, velit dolor tempor justo, eget posuere odio enim et purus. Pellentesque
-                    pharetra risus non ipsum vestibulum viverra. Proin nec augue lectus. Vivamus id hendrerit nunc.
-                    Donec imperdiet egestas lacinia.
-                </p>
-            </div>
+            <h1 class="heading-stylized">Droit et obligation lors de la creation d'un site</h1>
+            <p>Une personne voulant créer un site d’échange et de vente, peut s'orienter vers deux directions
+                différentes. Soit son but est non lucratif et veut juste faciliter la vie des utilisateurs, dans ce cas
+                précis, il crée une association. L'autre cas de figure serai de créer une entreprise affin de pouvoir
+                empocher des bénéfices en insérant des pubs ou en récupérant un pourcentage de chaque ventes par exemple
+                et ainsi pouvoir plus facilement rémunérer les employés.</p>
+            <h2 class="heading-stylized">Les modalités de la création d'une association</h2>
+            <h3>Les types d'associations</h3>
+            <p>D'abord, une association est une convention en vertu de laquelle deux personnes au moins décident de
+                partager leurs connaissance et/ou leurs activité dans le but autre que le partage des bénéfices ou la
+                recherche du profit. Il existe 5 types d'associations au total mais seuls 3 nous intéressent dans notre
+                cas.</p>
+            <ul>
+                <li>L’association déclarée qui nécessite l’accomplissement de certaines formalités administratives et
+                    doit être déclaré à la préfecture du département de son siège sociale ainsi que faire l’objet d’un
+                    avis de publication au journal officiel.
+                </li>
+                <li>L’association agrée dans lequel l’association reçois un agrément donné par décision ministérielle ou
+                    par une autorité administrative. Elle joui de nombreux avantages notamment au niveau financier car
+                    cet agrément lui permet de recevoir des subvention et des dons mais l’association doit posséder un
+                    numéro siret. Ce type d’association doit respecter plus d’obligation que des association classiques
+                    (comptabilité transparente).
+                </li>
+                <li>Association reconnue d’utilité publique qui permet de bénéficier d’une certaine notoriété ainsi que
+                    d’encourager les dons. Cependant elle doit respecter certaines conditions, doit avoir servi pour
+                    l’intérêt général et exister depuis au moins 3 ans.
+                </li>
+            </ul>
+            <h3>Permissions et restrictions de la loi de 1901</h3>
+            <p> La loi de 1901 permet, indépendamment de notre age, de notre nationalité ou de nos compétences, de créer
+                l’association de notre choix en bénéficiant d’avantages multiples et elle repose sur 3 principes
+                fondamentaux : la liberté contractuelle offrant la pleine liberté au membres de l’association quand a
+                son organisation, la gestion des intéressé c’est a dire que la recherche du profit et du partage des
+                bénéfices est interdite et la mise en commun des connaissance entre les membres de l’association. Cela
+                veut dire qu’elle ne dois pas payer d’impôts. Il faudra également rédiger les statut qui vont fixer les
+                règles de fonctionnement, de gouvernance et d’organisation et ces règles sont assez librement choisi par
+                ses membres. Cependant il est recommandé de mentionner le nom, l’objet (domaine d’activité et champ
+                d’action), l’addresses de son siège sociale, les conditions d’adhésion des membres, les règles de
+                fonctionnement. Enfin si elle le souhaite cette personne pourra créer un règlement intérieur cependant,
+                les sanctions devront être adapté aux erreurs des employés.</p>
+        </div>
+        <div class="col-12">
+            <h2 class="heading-stylized">Les modalités de la création d'une entreprise</h2>
+            <h3>Définition et début dans une entreprise</h3>
+            <p>Le deuxième consiste à créer une entreprise qui est une organisation ou une unité institutionnelle, mue
+                par un projet décliné en stratégie, en politiques et en plans d'action, dont le but est de produire et
+                de fournir des biens ou des services à destination d'un ensemble de clients ou d'usagers, en réalisant
+                un équilibre de ses comptes de charges et de produits. Pour, commencer, la personne vas créer une micro
+                entreprise personnelle ( biens personnels et bien de société confondus) et ceci en seulement quelques
+                clics. Celle-ci permet une comptabilité simplifiée avec comme simple obligation de déclarer son revenu
+                mensuel ou trimestriel et est souvent utilisé pour les personnes commençant une activité et ne sachant
+                pas si celle-ci va fonctionner. Si le chiffre d’affaire dépasse 82200 euros hors taxe pour l’achat
+                revente ou 32900 hors taxes pour les prestations de services, il faudra créer une société.</p>
+            <h3>Attribution de statut</h3>
+            <p>Si l'on veut créer une société, il faudra obligatoirement lui attribuer un statut. Chaque entreprise
+                possède un statut qui lui est attribué en fonction de nombreux facteurs. Ce statut détermine les règles
+                de fonctionnement de l’entreprise ainsi que les droits et les obligations des personnes qui la
+                composent. Voici les différents statuts que l’on peut choisir.</p>
+            <h4>EUR/SASU</h4>
+            <p>Une personne seule peut créer une entreprise unipersonnelle à responsabilité limitée (EUR) dans lequel le
+                PDG est non salarié et qui comporte plusieurs avantages, une création facile en ligne et capital maximal
+                exigé de 1 euro ou bien une Société par Action Simplifiée Unipersonnelle (SASU) dans lequel le PDG est
+                salarié (même statut sociale que les salariés) et on ne peut avoir qu’un seul associé au maximum,
+                l’avantage de celle-ci est que l’on peut cesser ses actions facilement.</p>
+            <h4>SARL/SAS/SA</h4>
+            <p>Une association de deux personnes minimum peuvent créer une société à responsabilité limitée (SARL) dans
+                laquelle les règles de fonctionnement seront fixés par la loi qui sont impossibles à modifier (par
+                exemple l’entrée d’un nouvel associé dans l’entreprise doit être accordé par la majorité des autres
+                associés) et le/les chefs ne seront pas salarié, ce type d’entreprise ressemble beaucoup à une EUR.</p>
+            <p>Pour un fonctionnement plus souple il faut transformer cette SARL en une Société par action simplifiée
+                (SAS) qui à pour avantage de favoriser l’entrée d’investisseurs extérieur dans l’entreprise (intéressant
+                si l’entreprise à besoin de fonds par exemple pour développer son activité à l’étranger ou autre)
+                cependant celle-ci offre des privilèges aux investisseurs comme le fait qu’en cas de faillite, ils
+                récupèrent l’argent investi. Dans ce statut, le/les chefs sont salariés.</p>
+            <p>Si le capital de l’entreprise atteint 37 000 euros, on peut créer une SA c'est-à-dire une Société Anonyme
+                qui permet de vendre des actions en bourse, dans notre cas qui concerne les sites d’échanges, le statut
+                de l'entreprise va dépendre du nombre d’associés qui sont derrière ce site, de la somme qu’il ou qu’ils
+                rapportent et de leurs ambitions, par exemple, s'ils ont prévu de s’étendre à l’étranger ou de changer
+                l'activité de leurs entreprises.</p>
         </div>
     </div>
 </main>
